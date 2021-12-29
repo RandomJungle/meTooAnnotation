17cd637edd6361dfb901817c4ab6410798b2ee10
Juliette <bourquinjuliette@gmail.com>
2021-12-29 11:38:37 +0100

    added french text option to interface

--- interface/app.py
+++ interface/app.py
@@ -1,12 +1,13 @@
 import json
+import os.path
 
 from flask import Flask, render_template, request
 from multiprocessing import Value
 
+import utils.paths as paths
 from annotator.annotate import reverse_label
 from utils.annotation_keys import TESTIMONY
 from utils.file_utils import read_tweet_from_path_and_index
-from utils.paths import INPUT_DATA_FILE, OUTPUT_DATA_FILE
 
 counter = Value('i', 0)
 app = Flask(__name__)
@@ -16,22 +17,22 @@ stored_data = []
 @app.route('/', methods=['GET', 'POST'])
 def index():
 
-    done_ids = extract_done_ids_from_output_file(OUTPUT_DATA_FILE)
-    tweet = read_tweet_from_path_and_index(INPUT_DATA_FILE, counter.value)
+    done_ids = extract_done_ids_from_output_file(paths.OUTPUT_DATA_FILE)
+    tweet = read_tweet_from_path_and_index(paths.INPUT_DATA_FILE, counter.value)
 
     # Listen for user input on saving data
     if request.method == "POST":
         if request.form['submit_button'] == 'save':
-            write_entries_to_file(OUTPUT_DATA_FILE, stored_data)
+            write_entries_to_file(paths.OUTPUT_DATA_FILE, stored_data)
             stored_data.clear()
 
     # read through file to find tweet not already done
     while tweet and tweet.get('id') in done_ids:
         counter.value += 1
-        tweet = read_tweet_from_path_and_index(INPUT_DATA_FILE, counter.value)
+        tweet = read_tweet_from_path_and_index(paths.INPUT_DATA_FILE, counter.value)
 
     # variable for the progress bar
-    len_progress_bar = calculate_progress_bar(INPUT_DATA_FILE)
+    len_progress_bar = calculate_progress_bar(paths.INPUT_DATA_FILE)
 
     # end of file reached
     if not tweet:
@@ -44,7 +45,7 @@ def index():
     label = TESTIMONY
     tweet_id = tweet.get('id')
     tweet_text = tweet.get('text')
-    tweet_english_text = tweet.get('english_text')
+    tweet_english_text = tweet.get('en_text')
 
     # user input in one of the three action buttons
     if request.method == "POST":
@@ -59,7 +60,7 @@ def index():
             with counter.get_lock():
                 counter.value += 1
                 out = counter.value
-            tweet = read_tweet_from_path_and_index(INPUT_DATA_FILE, out)
+            tweet = read_tweet_from_path_and_index(paths.INPUT_DATA_FILE, out)
             if not tweet:
                 return render_template(
                     'end.html',
@@ -69,7 +70,8 @@ def index():
                 'base.html',
                 tweet_id=tweet.get('id'),
                 tweet_text=tweet.get('text'),
-                tweet_english_text=tweet.get('english_text'),
+                tweet_english_text=tweet.get('en_text'),
+                tweet_french_text=tweet.get('fr_text'),
                 label=label,
                 index_progress=out,
                 progress_bar=len_progress_bar
@@ -82,12 +84,13 @@ def index():
                     with counter.get_lock():
                         counter.value -= 1
                         out = counter.value
-                    tweet = read_tweet_from_path_and_index(INPUT_DATA_FILE, out)
+                    tweet = read_tweet_from_path_and_index(paths.INPUT_DATA_FILE, out)
                     return render_template(
                         'base.html',
                         tweet_id=tweet.get('id'),
                         tweet_text=tweet.get('text'),
-                        tweet_english_text=tweet.get('english_text'),
+                        tweet_english_text=tweet.get('en_text'),
+                        tweet_french_text=tweet.get('fr_text'),
                         label=label,
                         index_progress=out,
                         progress_bar=len_progress_bar
@@ -98,6 +101,7 @@ def index():
         tweet_id=tweet_id,
         tweet_text=tweet_text,
         tweet_english_text=tweet_english_text,
+        tweet_french_text=tweet.get('fr_text'),
         label=label,
         index_progress=counter.value,
         progress_bar=len_progress_bar
@@ -117,7 +121,8 @@ def write_entries_to_file(file_path, buffer):
 
 def extract_done_ids_from_output_file(file_path):
     done_ids = []
-    with open(file_path, 'r') as output_file:
+    with open(file_path, 'a+') as output_file:
+        output_file.seek(0)
         for line in output_file.readlines():
             done_ids.append(json.loads(line).get('id'))
     return done_ids
